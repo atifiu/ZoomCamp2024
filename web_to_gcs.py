969e7140a575b3c06d66b8fc233ee96f49264fe9
Atif <48252551+atifiu@users.noreply.github.com>
2024-02-18 16:45:32 +0000

    Updated with upload of 2019_2020 taxi data

---
+++ web_to_gcs.py
@@ -0,0 +1,100 @@
+import io
+import os
+import requests
+import pandas as pd
+from google.cloud import storage
+
+"""
+Pre-reqs: 
+1. `pip install pandas pyarrow google-cloud-storage`
+2. Set GOOGLE_APPLICATION_CREDENTIALS to your project/service-account key
+3. Set GCP_GCS_BUCKET as your bucket or change default value of BUCKET
+"""
+
+# services = ['fhv','green','yellow']
+init_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/'
+# switch out the bucketname
+BUCKET = os.environ.get("GCP_GCS_BUCKET", "mage-atif-zoomcamp")
+
+
+def upload_to_gcs(bucket, object_name, local_file):
+    """
+    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
+    """
+    # # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    # storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    # storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
+
+
+def web_to_gcs(year, service):
+    try:
+        for i in range(12):
+            
+            # sets the month part of the file_name string
+            month = '0'+str(i+1)
+            month = month[-2:]
+
+            # csv file_name
+            file_name = f"{service}_tripdata_{year}-{month}.csv.gz"
+
+            # download it using requests via a pandas df
+            request_url = f"{init_url}{service}/{file_name}"
+            r = requests.get(request_url)
+            open(file_name, 'wb').write(r.content)
+            print(f"Local: {file_name}")
+            print(request_url)
+            taxi_dtypes = {
+            'VendorID': pd.Int64Dtype(),
+            'passenger_count': pd.Int64Dtype(),
+            'Trip_distance': float,
+            'PULocationID': pd.Int64Dtype(),
+            'DOLocationID': pd.Int64Dtype(),
+            'RatecodeID': pd.Int64Dtype(),
+            'Store_and_fwd_flag': str,
+            'payment_type': pd.Int64Dtype(),
+            'Fare_amount': float,
+            'Extra': float,
+            'MTA_tax': float,
+            'Improvement_surcharge': float,
+            'Tip_amount': float,
+            'Tolls_amount': float,
+            'Total_amount': float
+            #'trip_type': pd.Int64Dtype() commented for yellow taxi
+            }
+
+            parse_dates = ['tpep_pickup_datetime', 'tpep_dropoff_datetime']
+
+            # read it back into a parquet file
+            df = pd.read_csv(file_name, compression='gzip', dtype=taxi_dtypes, parse_dates=parse_dates, low_memory = False)
+            print("starting to load dataframe")
+            
+            #df = pd.read_csv(file_name, compression='gzip')
+            
+                
+            print(df.columns)
+            df['passenger_count'] = df['passenger_count'].astype('Int64')
+            df['payment_type'] = df['payment_type'].astype('Int64')
+            df['VendorID'] = df['VendorID'].astype('Int64')
+            df['RatecodeID'] = df['RatecodeID'].astype('Int64')
+            #df['trip_type'] = df['trip_type'].astype('Int64') commented for yellow taxi
+            file_name = file_name.replace('.csv.gz', '.parquet')
+            df.to_parquet(file_name, engine='pyarrow')
+            print(f"Parquet: {file_name}")
+
+            # upload it to gcs 
+            upload_to_gcs(BUCKET, f"{service}/{file_name}", file_name)
+            print(f"GCS: {service}/{file_name}")
+    except Exception as e:
+        print(e)
+
+
+#web_to_gcs('2019', 'green')
+#web_to_gcs('2020', 'green')
+#web_to_gcs('2019', 'yellow')
+web_to_gcs('2020', 'yellow')
